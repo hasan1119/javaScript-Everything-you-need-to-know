9219ca73d659e12f79837c53b999dab217b4cbce
Md. Rokibul Hasan <hasan.webappsdeveloper@gmail.com>
2021-08-10 23:23:21 +0600

    second session

--- javeScript-recap.js
+++ javeScript-recap.js
@@ -53,7 +53,7 @@ function testing() {
 
 testing() */
 
-
+/* 
 //Operators
 // 1. Arithmetic operators (+, - , * , /, % ,++ ,--)
 var x = 10;
@@ -71,4 +71,64 @@ b %= a;
 var a = 10;
 var b = "10";
 
-console.log(a === b);
+console.log(a === b); */
+
+/* 
+//Data types
+// 1. Primitive data types
+        1. number
+        2. string
+        3. boolean
+        4. undefined
+        5. null
+
+
+// 2. object data types 
+        1. Array
+        2. object
+        3. function
+*/
+
+
+// String methods and properties
+// var name = 'Hiro Alom';
+// var x = 100;
+// console.log(x);
+// x = String(x)
+// console.log(x);
+// console.log(name[2]);
+// console.log(name.length);
+// console.log(name.slice(5, 9));
+// console.log(name.slice(0, 4));
+// name = name.replace('Alom', 'sakib')
+// console.log(name);
+
+// console.log(name);
+// name = name.toUpperCase()
+// console.log(name);
+// name = name.toLowerCase()
+// console.log(name);
+
+// console.log("Hiro" + ' ' + "Alom");
+// console.log("Hiro " + "Alom");
+// console.log("Hiro", "Alom");
+
+// var str1 = "Hiro "
+// var str2 = "Alom "
+// console.log(str1.concat(str2));
+// var name = '                    Hiro Alom                             ';
+// console.log(name.trim());
+// var name = "Hiro"
+// name = name.padStart(10, 0)
+// console.log(name);
+// var name = "Hiro"
+// name = name.padEnd(10, 0)
+// console.log(name);
+
+// var name = 'Hiro Alom';
+// console.log(name.charAt(5));
+
+//charCodeAt(position)
+
+// var str = "amr sonar bangla ami tomay valobashi";
+// console.log(str.split(''));

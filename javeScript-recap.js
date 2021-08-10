dec7f6375bc702745fce4d626bde2dee1c802d0d
Md. Rokibul Hasan <hasan.webappsdeveloper@gmail.com>
2021-08-10 19:32:00 +0600

    session 1

---
+++ javeScript-recap.js
@@ -0,0 +1,74 @@
+/* // variable (var and let)
+var myVariable = 'first variable';
+let myVariable2 = 'second variable';
+
+// constant 
+const myConstant = 'my constant';
+
+// variable naming convention
+var myFirstVariable;
+var var1;
+var my_first_variable;
+var
+
+function;
+var $myVar;
+ */
+
+// Difference between variable and constant. 
+
+// // variable
+// let myVar = 10;
+// console.log(myVar);
+// myVar = 20;
+// console.log(myVar);
+
+// // constant
+// const x = 30;
+// console.log(x);
+
+// x = 50;
+// console.log(x);
+
+// const PI = 3.1416;
+
+// console.log(PI);
+// PI = 333333;
+
+// console.log(PI);
+
+// let x = 10; // global
+/* 
+function testing() {
+    if (true) {
+        // var x = 10;
+        let x = 10;
+        console.log(x);
+    }
+    if (true) {
+        console.log(x);
+    }
+    console.log(x);
+}
+
+testing() */
+
+
+//Operators
+// 1. Arithmetic operators (+, - , * , /, % ,++ ,--)
+var x = 10;
+// console.log(++x);
+
+var a = 10;
+var b = 20;
+b = b % a;
+// console.log(a);
+
+// 2. assignment operators (=, += , -=, *= , /= , %=)
+b %= a;
+
+// 3. comparison operators (== , ===,!= ,!==, < , >, <= , >=)
+var a = 10;
+var b = "10";
+
+console.log(a === b);
